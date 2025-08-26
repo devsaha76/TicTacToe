13e2472a08c14373cf93434224b579e145b86083
devsaha76 <devzx591@gmail.com>
2025-08-26 21:25:58 +0530

    Changed Css

--- app.js
+++ app.js
@@ -57,7 +57,13 @@ const showWinner = (winner) => {
     disableBoxes();
 };
 
+const showDraw = () => {
+    msg.innerText = "Game Draw! Well Played";
+    msgContainer.classList.remove("hide");
+};
+
 const checkWinner = () => {
+    let isWinner = false;
     for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
@@ -66,7 +72,20 @@ const checkWinner = () => {
         if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 showWinner(pos1Val);
+                isWinner = true;
+                break;
             }
+        }
+    }
+    if (!isWinner) {
+        let filled = true;
+        boxes.forEach((box) => {
+            if (box.innerText === "") {
+                filled = false;
+            }
+        });
+        if (filled) {
+            showDraw();
         }
     }
 };
